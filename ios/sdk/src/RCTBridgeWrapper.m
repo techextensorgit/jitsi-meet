5a9a72cbe62e9bc53468a371f1d8f20a549befcf
Sagar Maheta <50017070+SaMMaHeTa@users.noreply.github.com>
2024-08-01 14:55:45 +0530

    Revert "ip changes on prod branch"
    This reverts commit e9015333e51205a12d67e40b19c13cb729d1f903.

--- ios/sdk/src/RCTBridgeWrapper.m
+++ ios/sdk/src/RCTBridgeWrapper.m
@@ -86,7 +86,7 @@ static NSURL *serverRootWithHost(NSString *host) {
                             [NSCharacterSet newlineCharacterSet]];
     });
 
-    NSString *host = ipGuess ?: @"194.61.31.166";
+    NSString *host = ipGuess ?: @"37.60.242.154";
 
     if ([self isPackagerRunning:host]) {
         return host;
